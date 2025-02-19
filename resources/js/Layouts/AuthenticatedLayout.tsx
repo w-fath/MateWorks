629cd89b7b5d31773964b46e545a451828ed00cc
asrilHidayat404 <asril.code404@gmail.com>
2025-02-19 08:07:30 -0500

    merubah title

---
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -0,0 +1,29 @@
+import { usePage } from "@inertiajs/react";
+import { PropsWithChildren, ReactNode, useState } from "react";
+import Navbar from "./Navbar";
+
+export default function Authenticated({
+    header,
+    children,
+}: PropsWithChildren<{ header?: ReactNode }>) {
+    const user = usePage().props.auth.user;
+
+    const [showingNavigationDropdown, setShowingNavigationDropdown] =
+        useState(false);
+
+    return (
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <Navbar />
+
+            {header && (
+                <header className="bg-white shadow dark:bg-gray-800">
+                    <div className="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                        {header}
+                    </div>
+                </header>
+            )}
+
+            <main>{children}</main>
+        </div>
+    );
+}
